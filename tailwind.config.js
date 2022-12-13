9757c1a925d80531ea2b361e12b113b9e2a5e5c7
Ayush Soni <ayushsoni1010.work@gmail.com>
2022-12-14 04:33:00 +0530

    fix(cinfig): tailwindcss configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,15 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./common/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      "fontFamily": {
+      fontFamily: {
         "product-manrope": ["Manrope", "sans-serif"],
-        "product-bungee": ["Bungee", "cursive"]
+        "product-bungee": ["Bungee", "cursive"],
       },
-      "colors": {
+      colors: {
         "product-red": "#FF4E4E",
         "product-pink": "#FF008A",
         "product-orange": "#FF833E",
@@ -19,9 +19,9 @@ module.exports = {
         "product-teal": "#12C0AB",
         "product-yellow": "#FFB800",
         "product-blue": "#4E95FF",
-        "product-brown": "#231C18"
-      }
+        "product-brown": "#231C18",
+      },
     },
   },
   plugins: [],
-}
+};
