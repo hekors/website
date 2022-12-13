1b35eda0ab3e3de397c43b97579f924daccba8b5
Yash Sehgal <yashsehgal@Yashs-MacBook-Air.local>
2022-12-14 00:16:51 +0530

    feat(chore): skeleton loading modified

--- components/ui-patterns/Skeleton.tsx
+++ components/ui-patterns/Skeleton.tsx
@@ -1,16 +1,15 @@
-import Skeleton from 'react-loading-skeleton'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 import { CardSkeletonType } from '../../types/ui-pattern-types/skeleton-types';
 
 function CardSkeleton({ visibility }: CardSkeletonType) {
     return (
-        <Skeleton 
-            width="280px"
-            height="320px"
-            style={{
-                opacity: visibility
-            }}
-        />
+        <SkeletonTheme baseColor="#202020" highlightColor="#444">
+            <span>
+                <Skeleton height={320} width={280} style={{ opacity: visibility }} />
+                <Skeleton className="mt-4" height={20} width={280} style={{ opacity: visibility }} />
+            </span>
+        </SkeletonTheme>
     )
 }
 
