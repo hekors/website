f46e3723932cc9ee7d4bb1d2a5fdc687291672ef
Adarsh Dubey <dubeyadarshmain@gmail.com>
2022-12-25 00:42:49 +0530

    Added Head element so that title is visible

---
+++ components/MetaHead/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+const metaHead: React.FunctionComponent = () => {
+    return (
+          <Head>
+            <title>HEKORS Community</title>
+          </Head>
+      )
+}
+
+export default metaHead
