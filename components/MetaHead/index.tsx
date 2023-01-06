8c492cac26bcf69e2682f1e573869ffe7eed0b84
Yash Sehgal <yashsehgal@Yashs-MacBook-Air.local>
2023-01-07 00:37:10 +0530

    indentation fix

--- components/MetaHead/index.tsx
+++ components/MetaHead/index.tsx
@@ -1,15 +1,15 @@
 import Head from 'next/head'
 
 const MetaHead: React.FunctionComponent = () => {
-    return (
-          <Head>
-            <title>HEKORS Community</title>
-            <link rel="apple-touch-icon" sizes="180x180" href='/assets/favicon/apple-touch-icon.png' />
-            <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png" />
-            <link rel="manifest" href="/assets/favicon/site.webmanifest" />
-          </Head>
-      )
+  return (
+    <Head>
+      <title>HEKORS Community</title>
+      <link rel="apple-touch-icon" sizes="180x180" href='/assets/favicon/apple-touch-icon.png' />
+      <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png" />
+      <link rel="manifest" href="/assets/favicon/site.webmanifest" />
+    </Head>
+  )
 }
 
 export default MetaHead
